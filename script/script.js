08dc9093bc4615592e573c7cea588cb9fa8f217b
Alexander Kiryushkin <alexanderkiryushkin@yandex.ru>
2019-10-21 00:32:44 +0300

    сделал второе задание

--- script/script.js
+++ script/script.js
@@ -4,6 +4,26 @@ let addExpenses;
 let deposit;
 let mission;
 let period;
+let budgetDay;
 
-alert("Hello friends!!!");
-console.log("it will be fine");
+money = 45000; // Доход за месяц
+income = 'freelance';
+addExpenses = 'Internet, telephone, eat';
+deposit = true;
+mission = 300000;
+period = 12;
+budgetDay = money/30;
+
+console.log(typeof money);
+
+console.log(typeof income);
+
+console.log(typeof deposit);
+
+console.log(income.length);
+
+console.log("Период " + period + " месяцев" + " цель заработать " + mission + " рублей");
+
+console.log(addExpenses.toLowerCase(), addExpenses.split(", "));
+
+console.log("Результат: " + budgetDay, "остаток от деления: " + money % 30 );
