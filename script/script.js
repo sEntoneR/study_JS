41b25523e7812b0fe634767de10204bf3ff3f28f
Alexander Kiryushkin <alexanderkiryushkin@yandex.ru>
2019-10-23 00:00:37 +0300

    сделал третье задание

--- script/script.js
+++ script/script.js
@@ -1,18 +1,31 @@
-let money;
+'use strict';
+
+let money; 
 let income;
 let addExpenses;
-let deposit;
+let deposit; 
 let mission;
 let period;
 let budgetDay;
+let question1;
+let question2;
+let expense1;
+let expense2;
+let budgetMonth;
 
-money = 45000; // Доход за месяц
+money = +prompt('Ваш месячный доход?');
 income = 'freelance';
-addExpenses = 'Internet, Telephone, Eat';
-deposit = true;
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас депозит в банке?');
 mission = 300000;
-period = 12;
-budgetDay = money/30;
+question1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+expense1 = +prompt('Во сколько это обойдется?');
+question2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+expense2 = +prompt('Во сколько это обойдется?');
+budgetMonth = money - (expense1 + expense2);
+period = Math.ceil (mission / budgetMonth);
+budgetDay = Math.floor (budgetMonth / 30);
+
 
 console.log(typeof money);
 
@@ -20,10 +33,39 @@ console.log(typeof income);
 
 console.log(typeof deposit);
 
-console.log(income.length);
+// console.log(income.length);
 
-console.log("Период " + period + " месяцев" + " цель заработать " + mission + " рублей");
+// console.log("Период " + period + " месяцев" + " цель заработать " + mission + " рублей");
 
 console.log(addExpenses.toLowerCase(), addExpenses.split(", "));
 
-console.log("Результат: " + budgetDay, "остаток от деления: " + money % 30 );
+// console.log("Результат: " + budgetDay, "остаток от деления: " + money % 30 );
+
+
+
+console.log(question1);
+console.log(expense1);
+console.log(question2);
+console.log(expense2);
+console.log(period);
+console.log(budgetMonth);
+console.log(budgetDay);
+
+// if (!isNaN(parseFloat(expense1)) && isFinite(expense1)) {
+    
+//     alert('Введите число');
+// } else {
+//     console.log('число');
+// }
+
+if (budgetDay >= 800){
+    console.log('Высокий уровень дохода');
+} else if (800 > budgetDay >= 300){
+    console.log('Средний уровень дохода');
+} else if (300 > budgetDay === 0){
+    console.log('Низкий уровень дохода');
+} else {
+    console.log('Что то пошло не так');
+}
+
+// 
