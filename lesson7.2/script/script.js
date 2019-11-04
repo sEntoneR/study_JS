1f5d6361cadca8c3286523caa7ff81cc1ba9f79b
Alexander Kiryushkin <alexanderkiryushkin@yandex.ru>
2019-11-04 19:23:19 +0300

    седьмое задание

--- lesson7.2/script/script.js
+++ lesson7.2/script/script.js
@@ -1,50 +1,40 @@
 'use sctrict';
-
-let startButton = document.getElementById('start');
-console.log('startButton: ', startButton);
-
-let buttons = document.getElementsByTagName('button');
-console.log('buttons: ', buttons);
-
-let incomeButton = document.getElementsByTagName('button'[0]),
-    expensesButton = document.getElementsByTagName('button'[1]);
-
-let depositCheck = document.querySelector('#deposit-check');
-console.log('depositCheck: ', depositCheck);
-
-let incomeItem = document.querySelectorAll('.additional_income-item');
-console.log('incomeItem: ', incomeItem);
-
-
-
-let budgetDay = document.querySelector('.result-total budget_day-value'),
-    budgetMonth = document.querySelector('.result-total expenses_month-value'),
-    additionalIncome = document.querySelector('.result-total additional_income-value'),
-    additionalExpenses = document.querySelector('.result-total additional_expenses-value'),
-    incomePeriod = document.querySelector('.result-total income_period-value'),
-    targetMonth = document.querySelector('.result-total target_month-value');
-
-console.log('budgetDay: ', budgetDay);
-console.log('budgetMonth: ', budgetMonth);
-console.log('additionalIncome: ', additionalIncome);
-console.log('additionalExpenses: ', additionalExpenses);
-console.log('incomePeriod: ', incomePeriod);
-console.log('targetMonth: ', targetMonth);
-
-let salaryAmount = document.querySelector('.salary-amount'),
+let start = document.getElementById('start'),
+    buttons = document.getElementsByTagName('button'),
+    incomeButton = buttons[0],
+    expensesButton = buttons[1],
+    depositCheck = document.querySelector('#deposit-check'),
+    incomeItem = document.querySelectorAll('.additional_income-item'),
+    budgetDayValue = document.getElementsByClassName('budget_day-value')[0],
+    budgetMonthValue = document.getElementsByClassName('budget_month-value')[0],
+    expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0],
+    accumulatedMonthValue = document.getElementsByClassName('accumulated_month-value')[0],
+    additionalIncome = document.getElementsByClassName('additional_income-value')[0],
+    additionalExpenses = document.getElementsByClassName('additional_expenses-value')[0],
+    incomePeriod = document.getElementsByClassName('income_period-value')[0],
+    targetMonth = document.getElementsByClassName('target_month-value')[0],
+    salaryAmount = document.querySelector('.salary-amount'),
     incomeTitle = document.querySelector('.income-title'),
     incomeAmount = document.querySelector('.income-amount'),
     expensesTitle = document.querySelector('.expenses-title'),
     expensesAmount = document.querySelector('.expenses-amount'),
-    additionalExpensesItem = document.querySelector('.additional_expenses-item'),   
-    targetAmount = document.querySelector('.target-amount');
-    
-
-    console.log('salaryAmount: ', salaryAmount);
-    console.log('incomeTitle: ', incomeTitle);
-    console.log('incomeAmount: ', incomeAmount);
-    console.log('expensesTitle: ', expensesTitle);
-    console.log('expensesAmount: ', expensesAmount);
-    console.log('additionalExpensesItem: ', additionalExpensesItem);
+    additionalExpensesItem = document.querySelector('.additional_expenses'),   
+    periodSelect = document.querySelector('.period-select');
+console.log('depositCheck: ', depositCheck);
+console.log('startButton: ', start);
+console.log('buttons: ', buttons);
+console.log('incomeItem: ', incomeItem);
+console.log('budgetDay: ', budgetDayValue);
+console.log('budgetMonth: ', budgetMonthValue);
+console.log('additionalIncome: ', additionalIncome);
+console.log('additionalExpenses: ', additionalExpenses);
+console.log('incomePeriod: ', incomePeriod);
+console.log('targetMonth: ', targetMonth);
+console.log('salaryAmount: ', salaryAmount);
+console.log('incomeTitle: ', incomeTitle);
+console.log('incomeAmount: ', incomeAmount);
+console.log('expensesTitle: ', expensesTitle);
+console.log('expensesAmount: ', expensesAmount);
+console.log('additionalExpensesItem: ', additionalExpensesItem);
 
 
