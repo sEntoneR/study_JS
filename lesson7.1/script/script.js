32dd7cb282c90f89dd5295fb63fcb1934172bf00
Alexander Kiryushkin <alexanderkiryushkin@yandex.ru>
2019-11-05 20:36:49 +0300

    поправил седьмое задание

--- lesson7.1/script/script.js
+++ lesson7.1/script/script.js
@@ -24,11 +24,34 @@ image.setAttribute('style', 'background-image:url(/image/you-dont-know-js.jpg)')
 //исправление названия книги 
 let book = document.querySelectorAll('.book');
 console.log('book: ', book);
-// book[2].innerHTML = 'Книга 3. this и Протопипы Объектов';
-let newElem = document.createElement('h2');
-newElem.textContent = 'Книга 3. this и Протопипы Объектов';
-book[2].appendChild(newElem); 
+book[2].querySelector('a').textContent = 'Книга 3. this и Протопипы Объектов';
 
+// книга 2
+let chapter = book[1].querySelectorAll('li'),
+    ul = book[1].querySelector('ul');
+
+    ul.insertBefore(chapter[6], chapter[4]);
+    ul.insertBefore(chapter[8], chapter[4]);
+    ul.insertBefore(chapter[2], chapter[10]);
+
+// книга 5
+
+let _chapter = book[4].querySelectorAll('li'),
+    _ul = book[4].querySelector('ul');
+
+    _ul.insertBefore(_chapter[9], _chapter[2]);
+    _ul.insertBefore(_chapter[3], _chapter[2]);
+    _ul.insertBefore(_chapter[4], _chapter[2]);
+    _ul.insertBefore(_chapter[6], _chapter[5]);
+    _ul.insertBefore(_chapter[7], _chapter[5]);
+
+//добавление главы
+
+let liBook = book[5].querySelectorAll('li'),
+    ulBook = book[5].querySelector('ul'),
+    newElem = document.createElement('li');
+    newElem.textContent = 'Глава 8: За пределами ES6';
+    ulBook.insertBefore(newElem, liBook[9]);
 
 
 
