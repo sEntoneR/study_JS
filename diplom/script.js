f378ffd47e97e78e03f82177d8888f519d5db82a
Alexander Kiryushkin <alexanderkiryushkin@yandex.ru>
2019-12-22 21:12:49 +0300

    diplom

---
+++ diplom/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// popup
+
+const togglePopUp = () => {
+// const popups = document.querySelectorAll('.popup');
+let isModalOpened = false;
+let modal = null;
+
+  document.body.addEventListener ('click', () => {
+
+  const element = event.target;
+  const elementClasses = element.classList.toString();
+
+
+
+  if (elementClasses.match(/discount|call|check/) && !isModalOpened){
+     const modalClass = `.popup-${elementClasses.match(/discount|call|check/)[0]}`;
+     modal = document.querySelector(modalClass);
+     modal.classList.add('popup--show');
+     isModalOpened = true;
+
+  }
+
+  if (element.classList.contains('popup-close') || isModalOpened && element.classList.contains('popup--show')){
+      // popups.forEach((popup) => popup.classList.remove('popup--show'));
+      modal.classList.remove('popup--show');
+      isModalOpened = false;
+  } 
+});
+};
+
+togglePopUp();
