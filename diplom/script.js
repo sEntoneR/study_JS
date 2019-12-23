22f3586568901dfe24a5facbeb539044322962a0
Alexander Kiryushkin <alexanderkiryushkin@yandex.ru>
2019-12-23 22:25:38 +0300

    сделал аккардеон

--- diplom/script.js
+++ diplom/script.js
@@ -32,6 +32,8 @@ let modal = null;
 
 togglePopUp();
 
+// addbutton
+
 const addButton = () => {
 const hiddenBlocks = document.querySelectorAll('.hidden'),
       addSentenceBtn = document.querySelector('.add-sentence-btn');
@@ -45,3 +47,38 @@ const hiddenBlocks = document.querySelectorAll('.hidden'),
 };
 
 addButton();
+
+// аккордион 
+
+const accordionTwo = (elem) => {
+  const accordionTwo = document.getElementById('accordion-two'),
+        panelHeading = accordionTwo.querySelectorAll('.panel-heading'),
+        panelCollapse = accordionTwo.querySelectorAll('.panel-collapse');
+
+  const togglePanel = (index) => {
+    for (let i = 0; i < panelCollapse.length; i++) {
+    if (index === i) {
+      panelCollapse[i].classList.add('in');
+    } else {
+      panelCollapse[i].classList.remove('in');
+    }
+  }
+};
+
+accordionTwo.addEventListener('click', (event) => {
+  let target = event.target;
+  target = target.closest('.panel-heading');
+  if (target) {
+    panelHeading.forEach((item, i) => {
+      event.preventDefault();
+      if (item === target) {
+        togglePanel(i);
+      }
+    });
+  }
+});
+
+
+};
+
+accordionTwo();
